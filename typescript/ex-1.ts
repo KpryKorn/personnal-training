c1f9bb5587ebe96ea2d66d64ba8d0d155d4e57f5
Sacha <sroffini@mgen.fr>
2024-10-14 14:30:12 +0200

    add first exercise

--- typescript/ex-1.ts
+++ typescript/ex-1.ts
@@ -1,1 +1,14 @@
-// init
+// Exercice 1 : Manipulation de chaînes de caractères
+// Crée une fonction en TypeScript qui prend une chaîne de caractères en paramètre et renvoie la chaîne inversée. Assure-toi de bien typer la fonction et de gérer les cas où la chaîne est vide.
+
+function reverseString(str: string): string {
+  if (!str) {
+    return "You must provide a string in order to reverse it.";
+  }
+
+  const reversedString = str.split("").reverse().toString().replaceAll(",", "");
+
+  return reversedString;
+}
+
+console.log(reverseString("Salut"));
